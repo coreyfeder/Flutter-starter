5c35d08cca9283db96a5bc7b3b0ab3ed8ebdb1a7
Corey Feder <corey.feder@gmail.com>
2024-11-18 16:00:13 -0500

    build: better color

--- flutter_app_1/lib/main.dart
+++ flutter_app_1/lib/main.dart
@@ -19,7 +19,7 @@ class MyApp extends StatelessWidget {
         title: 'Flutter Demo: Namer App',
         theme: ThemeData(
           useMaterial3: true,
-          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepOrange),
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.purple),
         ),
         home: MyHomePage(),
         // home: const MyHomePage(title: 'Flutter Demo Home Page'),  // tutorial version
